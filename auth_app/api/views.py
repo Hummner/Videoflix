bd7bf3cef5e2017b60f982741d472813554cc468
Hummner <bence.cservenyak@gmail.com>
2026-01-06 07:42:13 +0100

    fix: user in RegisterView

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,7 +1,17 @@
 from rest_framework.views import APIView
+from .serializers import RegisterSerializer
 
 
 class RegisterView(APIView):
 
     def post(self, request):
+        serialiezer = RegisterSerializer(data=request.data)
+
+        serialiezer.is_valid(raise_exception=True)
+        user = serialiezer.save()
+
+        self.send_activation_email(request, user)
+
+
+    def send_activation_email(self, request, user):
         pass
