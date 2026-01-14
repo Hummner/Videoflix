673317ba69973ceab82f0f9846488bc02f5eb416
Hummner <bence.cservenyak@gmail.com>
2026-01-14 07:36:38 +0100

    fix: path problem by 480p converter

--- video_app/converter/hls_converter.py
+++ video_app/converter/hls_converter.py
@@ -1,32 +1,36 @@
 import subprocess
 from django.conf import settings
 from pathlib import Path
+from django.utils.text import slugify
 
 
 
 class ConvertVideoToHls():
 
-    def __init__(self, video_titel, video_name):
-        self.video_titel = video_titel
-        self.video_name = video_name
+    def __init__(self, video_path, title):
+        # self.title = slugify(title) kesobb!!!!
+        self.title = title
+        self.video_path = Path(video_path)
+        self.video_name = self.video_path.name
+        self.video_stem = self.video_path.stem
+        self.video_parent = self.video_path.parent
+        self.video_suffix = self.video_path.suffix
 
     def convert_video_480p(self):
-        media_root = Path(settings.MEDIA_ROOT)
-
-        input_video = media_root / "video" / self.video_titel / self.video_name
-
-        hls_dir = media_root / "video" / self.video_titel / "HLS"
+        hls_dir = Path(settings.MEDIA_ROOT) / "videos" / self.title / "hls" / "480p"
         hls_dir.mkdir(parents=True, exist_ok=True)
 
-        output_m3u8 = hls_dir / f"{Path(self.video_name).stem}.m3u8"
-        segment_pattern = hls_dir / f"{Path(self.video_name).stem}%d.ts"
+        output_m3u8 = hls_dir / f"{self.video_stem}.m3u8"
+        segment_pattern = hls_dir / f"{self.video_stem}%d.ts"
+
+
 
 
         result = subprocess.run(
                     [
                         "ffmpeg",
                         "-y",
-                        "-i", str(input_video),
+                        "-i", str(self.video_path),
                         "-vf", "scale=-2:480",
                         "-c:v", "libx264",
                         "-crf", "23",
--- video_app/signals.py
+++ video_app/signals.py
@@ -2,15 +2,17 @@ from django.db.models.signals import post_save
 from .models import Video
 from django.dispatch import receiver
 from .converter.hls_converter import ConvertVideoToHls
+from pathlib import Path
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print("Video saved")
+    print("name: ", instance.video_file.name)
+    print("name: ", instance.video_file.path)
 
     if created:
-        video_name = instance.video_file.name
-        video_title = instance.title
-        video = ConvertVideoToHls(video_title, video_name)
+        print("Convert staring...")
+        video = ConvertVideoToHls(instance.video_file.path, instance.title)
         video.convert_video_480p()
 
         ##was macht geanu pathlib
